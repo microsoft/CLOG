aee2b8cdc868fb4cce288297a3a90df350d48e93
chgray <chgray@microsoft.com>
2020-03-12 05:55:52 -0700

    experimenting with simple example

--- examples/clogsample/simple.cpp
+++ examples/clogsample/simple.cpp
@@ -5,6 +5,14 @@
 int main(int argc, char* argv[])
 {
     printf("Hello world!");
-    TraceInfo(Launched, "Hello world - we just started %d", 80);
+    TraceInfo(LAUNCHED, "Hello world - we just started %d", 80);
+                
+    char *buffer = new char[10];
+    for(char i=0; i<10; ++i)
+    {
+        TraceInstanceInfo(INSTANCE_TEST, buffer, "1:%d 2:%s 3:%c 4:%u 5:%u 6:%u", 1, "2", 3, 4, 5, 6);        
+    }
+    delete [] buffer;
+
     return 0;
 }
