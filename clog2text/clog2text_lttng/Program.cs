49cdf6f2838c32a9e3d9afbe856146e0076d1431
chgray <chgray@microsoft.com>
2020-04-09 10:47:24 -0700

    dont crash if we're out emitting to a file

--- clog2text/clog2text_lttng/Program.cs
+++ clog2text/clog2text_lttng/Program.cs
@@ -116,8 +116,11 @@ namespace clog2text_lttng
                     DecodeAndTraceToConsole(outputfile, bundle, line, config, valueBag);
                 }
 
-                outputfile.Flush();
-                outputfile.Close();
+                if (null != outputfile)
+                {
+                    outputfile.Flush();
+                    outputfile.Close();
+                }
 
                 Console.WriteLine($"Decoded {lines} in {DateTimeOffset.Now - startTime}");
                 return 0;
